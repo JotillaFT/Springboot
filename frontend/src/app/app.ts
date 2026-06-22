7ea1359b9742f3c498bae70b68c505e3cd244302
Jotilla <jfonttroi@gmail.com>
2026-06-23 01:51:39 +0200

    Añade frontend Angular inicial conectado al backend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit, signal } from '@angular/core';
 import { Usuario } from './model/usuario';
 import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
-  usuarios: Usuario[] = [];
+  usuarios = signal<Usuario[]>([]);
 
   constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
     console.log('APP INICIADA');
+
     this.usuarioService.obtenerUsuarios().subscribe({
       next: (datos) => {
-        this.usuarios = datos;
+        console.log('USUARIOS RECIBIDOS', datos);
+        this.usuarios.set(datos);
+        console.log('TOTAL GUARDADO', this.usuarios().length);
       },
       error: (error) => {
         console.error(error);
